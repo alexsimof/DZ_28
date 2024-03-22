62998fe73c8ff616bfeb4bdca47b13b6eda77405
Alex Simonov <alex.simonof@gmail.com>
2024-03-22 22:19:53 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -40,9 +40,37 @@ function getDurationTime(time1, time2) {
 
 function getYourBd(dateBD) {
   let now = new Date();
-  let year = Date.parse(now) - Date.parse(dateBD)
+  let data = Date.parse(now) - Date.parse(dateBD)
 
-  return year = Math.ceil(year);
+  return year = Math.floor(data / (1000 * 60 * 60 * 24 * 30 * 12));
 }
-console.log(getYourBd('1976.08.23'))
+// console.log(getYourBd('1983-05-15'))
+
+// task 4
+
+function countDown(data) {
+  let dt = Date.now();
+  let newTime = new Date(data).getTime();
+  let out = newTime - dt;
+  let day = Math.floor(out / (1000 * 60 * 60 * 24) % 30)
+  let year = Math.floor(out / (1000 * 60 * 60 * 24 * 30 * 12))
+  let month = Math.floor(out / (1000 * 60 * 60 * 24 * 30) % 12)
+
+  return {day: day, month: month, year: year}
+}
+
+// console.log(countDown('2025-05-10'))
+
+// task 5
+
+function deadline (timeout) {
+  let dt = Date.now();
+  let dl = new Date(timeout).getTime();
+  let timeCheck = dl - dt;
+  let day = Math.floor(timeCheck / (1000 * 60 * 60 * 24));
+  return day
+
+}
+
+// console.log(deadline('2024-12-05'))
 
